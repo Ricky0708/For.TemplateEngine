a8f1a19acd79349e22895bd36d73464ed804c667
07450.ricky.huang <07450.ricky.huang@ICT888.NET>
2018-04-24 19:22:02 +0800

    unittest

---
+++ test/UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("426c6786-f3b3-4f92-8f20-8a284d0485f2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ test/UnitTest/UnitTest1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using For.TemplateParser;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PerformancTest()
+        {
+            var template = "Hi! {.Name}, your age is {.Age}, {.StandardDateTime}, {.OffsetDateTime}";
+            var obj = new TestModel()
+            {
+                Name = "Ricky",
+                Age = 25,
+                StandardDateTime = DateTime.Parse("2017/08/01"),
+                OffsetDateTime = DateTimeOffset.Parse("2017/08/02")
+            };
+            Stopwatch watch = new Stopwatch();
+            watch.Start();
+            string resultA = "";
+            Parallel.For(0, 1000000, p =>
+            {
+                resultA = TemplateParserProvider.BuildTemplate(obj, template);
+                //TemplateParserProvider.ClearCaches();
+                if (!resultA.StartsWith("Hi!"))
+                {
+                    throw new Exception();
+                }
+            });
+            watch.Stop();
+            Assert.IsTrue(watch.ElapsedMilliseconds < 2000);
+            Assert.AreEqual(
+                resultA,
+                "Hi! Ricky, your age is 25, 8/1/2017 12:00:00 AM, 8/2/2017 12:00:00 AM +08:00");
+        }
+    }
+    public class TestModel
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime StandardDateTime { get; set; }
+        public DateTimeOffset OffsetDateTime { get; set; }
+    }
+}
